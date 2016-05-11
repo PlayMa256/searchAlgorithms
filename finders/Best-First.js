8c4446fe581d24549c21726ebe29773995347238
PlayMa256 <matheus.g.silva@hotmail.com>
2016-05-11 17:56:30 -0300

    best-first criado

---
+++ finders/Best-First.js
@@ -0,0 +1,32 @@
+function BestFirst(options){
+	this.heuristica = options.heuristica;
+}
+BestFirst.prototype.findPath = function(nodeInicial, nodeFinal, Grid){
+	var node, i, j, k, x, y, vizinhos, vizinhoG, vizinho;
+	var abertos = new Heap(function(nodeA, nodeB){
+		nodeA.f - nodeB.f;
+	});
+
+	nodeInicial.f = 0;
+	abertos.push(nodeInicial);
+	while(!abertos.empty()){
+		node = abertos.pop();
+
+		if(node === nodeFinal){
+			return 0;
+		}
+
+		vizinhos = Grid.getNeighbors(node, true);
+		for(i=0;i<vizinhos.length;i++){
+			vizinho = vizinhos[i];
+			x = vizinho.x;
+			y = vizinho.y;
+			vizinhos.f = this.heuristica(x, nodeFinal.x, y, nodeFinal.y);
+			vizinhos.parent = node;
+			abertos.push(vizinhos);
+		}
+
+
+	}
+
+};
