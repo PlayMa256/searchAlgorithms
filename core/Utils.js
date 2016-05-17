11b162ef2fc69045ee61109b584760a5e28baba3
Andrewmat <=>
2016-05-16 23:14:42 -0300

    Criacao de grid no DOM

--- core/Utils.js
+++ core/Utils.js
@@ -1,21 +1,45 @@
 
 function backtrace(node) {
-    var path = [[node.x, node.y]];
+    var path = [{'x' : node.x, 'y' : node.y}];
     while (node.parent) {
         node = node.parent;
-        path.push([node.x, node.y]);
+        path.push({'x' : node.x, 'y' : node.y});
     }
     return path.reverse();
 }
 
-function createDomGrid(grid){
+function criarGrid(grid){
     var container = document.getElementById('container');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
     var celula = document.createElement('div');
     celula.classList.add('celula');
+
     for (i = 0; i < grid.altura; i++) {
         for (j = 0; j < grid.largura; j++) {
-            container.appendChild(celula.cloneNode());
+            var clone = celula.cloneNode();
+            if (!grid.isTransitavel(j, i)) {
+                clone.classList.add('bloqueada');
+            }
+            container.appendChild(clone);
         }
-
+        container.appendChild(document.createElement('br'));
     }
+}
+function pintarGrid(grid, caminho) {
+    criarGrid(grid)
+    caminho.forEach(function(ponto) {
+        try {
+            pintarCelula(grid, ponto);
+        } catch(err) {
+            console.error('Pintura de ' + ponto.x + ',' + ponto.y + ' nao foi bem sucedida.');
+        }
+    })
+}
+function pintarCelula(grid, ponto) {
+    var ind = grid.largura * ponto.y + ponto.x;
+    var celula = document.querySelectorAll('.celula')[ind];
+    celula.classList.add('is-pintada');
 }
